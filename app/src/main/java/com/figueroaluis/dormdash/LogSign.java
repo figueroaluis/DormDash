0fc9e0071f4bdaf8f629c56f6e010eb3b5ef2b65
Luis Figueroa <figueroa.ides@gmail.com>
2019-04-09 15:51:14 -0700

    update log/sign page

--- app/src/main/java/com/figueroaluis/dormdash/LogSign.java
+++ app/src/main/java/com/figueroaluis/dormdash/LogSign.java
@@ -1,20 +1,26 @@
 package com.figueroaluis.dormdash;
 
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class LogSign extends AppCompatActivity implements View.OnClickListener{
+public class LogSign extends AppCompatActivity implements View.OnClickListener, View.OnKeyListener{
 
     // check to see if we are in sign up or log in
     Boolean signUpMode = true;
     // buttons and textviews
     TextView loginTextView;
+    EditText usernameEditText;
+    EditText passwordEditText;
 
 
     // implement a function that allows the button to open another view
@@ -38,21 +44,21 @@ public class LogSign extends AppCompatActivity implements View.OnClickListener{
                 signUpButton.setText("Sign Up");
                 loginTextView.setText("or, Log In");
             }
+        } else if(view.getId() == R.id.dormdash_logo || view.getId() == R.id.logSign_backgroundLayout){
+            InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),0);
         }
     }
 
+    // method to sign up
     public void onSignUpClicked(View view){
-        // get access to the edit texts in the log in page
-        EditText usernameEditText = findViewById(R.id.username_editText);
-        EditText passwordEditText = findViewById(R.id.password_editText);
-
-
         // make sure that we have both a username and a password
-        if(usernameEditText.getText().toString().matches("") || passwordEditText.getText().toString().matches("")){
-            Toast.makeText(this, "A username and a password is required.", Toast.LENGTH_SHORT).show();
+        if(usernameEditText.getText().toString().equals("") || passwordEditText.getText().toString().equals("")){
+            Toast.makeText(LogSign.this, "A username and a password are required.", Toast.LENGTH_SHORT).show();
         } else{
             // if the username or the password is in there, then we set them up
             // this is the sign up call
+            Toast.makeText(LogSign.this, "A username and a password have been given.", Toast.LENGTH_SHORT).show();
             if(signUpMode){
 
             }else{
@@ -60,6 +66,14 @@ public class LogSign extends AppCompatActivity implements View.OnClickListener{
             }
         }
 
+    }
+
+    @Override
+    public boolean onKey(View view, int i, KeyEvent keyEvent){
+        if(i == KeyEvent.KEYCODE_ENTER && keyEvent.getAction() == KeyEvent.ACTION_DOWN){
+            onSignUpClicked(view);
+        }
+        return false;
     }
 
 
@@ -74,6 +88,19 @@ public class LogSign extends AppCompatActivity implements View.OnClickListener{
         // access the textview of log in
         loginTextView = findViewById(R.id.logIn_textView);
         loginTextView.setOnClickListener(this);
+        // get access to the edit texts in the log in page
+        usernameEditText = findViewById(R.id.username_editText);
+        passwordEditText = findViewById(R.id.password_editText);
+        ImageView logoImageView = findViewById(R.id.dormdash_logo);
+        ConstraintLayout backgroundLayout = findViewById(R.id.logSign_backgroundLayout);
+        // getting rid of the keyboard, clicking elsewhere
+        logoImageView.setOnClickListener(LogSign.this);
+        backgroundLayout.setOnClickListener(LogSign.this);
+
+        // if we click on enter on the keyboard and then disappear the keyboard
+        // it allows us to log in
+        passwordEditText.setOnKeyListener(LogSign.this);
+
 
     }
 
