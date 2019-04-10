b1899dd9daa717ec29a112a307f01d4a6d3b7cee
Sam <sym2015@gmail.com>
2019-04-10 14:40:23 -0700

    can access home screen and profile screen from order screen now with the bottom bar buttons

--- app/src/main/java/com/figueroaluis/dormdash/OrderScreen.java
+++ app/src/main/java/com/figueroaluis/dormdash/OrderScreen.java
@@ -1,13 +1,37 @@
 package com.figueroaluis.dormdash;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
-public class OrderScreen extends AppCompatActivity {
+public class OrderScreen extends AppCompatActivity implements View.OnClickListener {
+
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.imageButton_profile) {
+            // log for us to check
+            Log.i("Open Sign Up", "Was pressed");
+            Intent intent = new Intent(OrderScreen.this, LogSign.class);
+            startActivity(intent);
+        }
+        else if(view.getId() == R.id.imageButton_homebutton) {
+            Log.i("Open Sign Up", "Was pressed");
+            Intent intent = new Intent(OrderScreen.this, MainActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    ImageButton homeButton;
+    //ImageButton searchButton;
+    //ImageButton ordersButton;
+    ImageButton profileButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +53,12 @@ public class OrderScreen extends AppCompatActivity {
 
             }
         });
+
+        profileButton = (ImageButton) findViewById(R.id.imageButton_profile);
+        profileButton.setOnClickListener(OrderScreen.this);
+
+        homeButton = (ImageButton) findViewById(R.id.imageButton_homebutton);
+        homeButton.setOnClickListener(OrderScreen.this);
 
     }
 }
