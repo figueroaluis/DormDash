3ed60e63647ef8836c09307308b94bba9450e5ce
Sam <sym2015@gmail.com>
2019-04-15 18:16:56 -0700

    Search Screen, but it works this time

--- app/src/main/java/com/figueroaluis/dormdash/SearchScreen.java
+++ app/src/main/java/com/figueroaluis/dormdash/SearchScreen.java
@@ -31,15 +31,17 @@ public class SearchScreen extends AppCompatActivity implements View.OnClickListe
 
     }
 
+    ImageButton homeButton;
+    ImageButton searchButton;
+    ImageButton orderButton;
+    ImageButton profileButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_screen);
 
-        ImageButton homeButton;
-        ImageButton searchButton;
-        //ImageButton orderButton;
-        ImageButton profileButton;
+
 
         profileButton = (ImageButton) findViewById(R.id.imageButton_profile);
         profileButton.setOnClickListener(SearchScreen.this);
@@ -49,6 +51,9 @@ public class SearchScreen extends AppCompatActivity implements View.OnClickListe
 
         searchButton = (ImageButton) findViewById(R.id.imageButton_search);
         searchButton.setOnClickListener(SearchScreen.this);
+
+        orderButton = (ImageButton) findViewById(R.id.imageButton_orders);
+        orderButton.setOnClickListener(SearchScreen.this);
 
     }
 }
