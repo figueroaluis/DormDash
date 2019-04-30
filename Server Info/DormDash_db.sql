0b86d5815f4632193419226d44b73fc486130cb7
Sam <sym2015@gmail.com>
2019-04-29 17:16:54 -0700

    /order, /cancel order, and locations table
    Attempted /order and /cancelorder endpoints, but Im not sure if I am doing it properly or am on the right track. Also filled the locations table with all the res halls

--- Server Info/DormDash_db.sql
+++ Server Info/DormDash_db.sql
@@ -23,17 +23,31 @@ create table locations (
 
 create table orders (
 	username varchar(40) not null,
-	orderID int not null,
-	foodOrder varchar(50),
+	orderID int not null auto_increment,
+	foodOrder varchar(100),
 	orderPickupLocation varchar(40),
 	orderDropoffLocation varchar(40),
 	pickupTime time,
 	dropoffTime time,
 	primary key(orderID),
 	foreign key(username) references users(username),
-	foreign key(workerID) references workers(workerID),
 	foreign key(orderPickupLocation) references locations(buildingName),
 	foreign key(orderDropoffLocation) references locations(buildingName));
+
+INSERT INTO locations VALUES ("Berkus Hall", 3.00),
+("Braun Hall", 3.00),
+("Norris Hall", 3.00),
+("Stearns Hall", 3.00),
+("Stewart-Cleland Hall", 3.00),
+("Pauley Hall", 2.00),
+("Bell-Young Hall", 2.00),
+("Erdman Hall", 2.00),
+("Wylie Hall", 2.00),
+("Newcomb Hall", 2.00),
+("Chilcott Hall", 1.00),
+("Haines Hall", 1.00);
+
+
 
 
 
